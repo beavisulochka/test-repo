235114508a0e330655be875ffa4860ab0f9f8b33
beavisulochka <rodionmodelist@gmail.com>
2022-12-19 19:02:40 +0100

    Add code

---
+++ UserCheck/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace UserCheck
+{
+	public class User
+	{
+		public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User()
+		{
+            AccountCreate();
+        }
+
+		public virtual void AccountCreate()
+		{
+			Console.WriteLine("Hello, write your user name please");
+			Name = Console.ReadLine();
+            Console.WriteLine("Yep, name was saved.");
+
+            Console.WriteLine("Now time for your password(must be eight symbols min).");
+            string code = Console.ReadLine();
+
+            while (code.Length < 8)
+            {
+                Console.WriteLine("Password must be eight symbols min, write your password again please");
+
+
+                if (code.Length >= 8)
+                {
+                    Console.WriteLine("Great, confrim password please.");
+                    string confrim = Console.ReadLine();
+
+                    while (confrim != code)
+                    {
+                        Console.WriteLine("Code not right, try again:(");
+                        confrim = Console.ReadLine();
+
+                        if (confrim == code)
+                        {
+                            confrim = Password;
+                            Console.WriteLine("Great, your password saved!.");
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+	}
+}
+
---
+++ UserCheck/UserPro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace UserCheck
+{
+	public class UserPro : User
+	{
+		public string Email { get; set; }
+		public int PostCode { get; set; }
+
+		public UserPro() : base()
+		{
+			AccountCreate();
+		}
+
+        public override void AccountCreate()
+        {
+            base.AccountCreate();
+			EmailCreate();
+			PostCodeCreate();
+
+        }
+
+		public void EmailCreate()
+		{
+			Console.WriteLine("Wite your email, email adress which you want to connect to your account");
+			string answer = Console.ReadLine();
+			string dog = "@";
+			string dot = ".";
+ 			while (!answer.Contains(dog) && !answer.Contains(dot)) 
+			{
+				Console.WriteLine("Usualy email have dots and '@' symbols, try to write email again");
+                answer = Console.ReadLine();
+
+                if (answer.Contains(dog) && answer.Contains(dot))
+				{
+                    Email = answer;
+                    Console.WriteLine("Your email was saved.");
+					break;
+                }
+            }
+        }
+
+		public void PostCodeCreate()
+		{
+            Console.WriteLine("Wite your postcode for shipping information");
+            int answer = int.Parse(Console.ReadLine());
+
+            while (answer.ToString().Length > 5 && answer.ToString().Length < 5)
+			{
+                Console.WriteLine("Try again, postcode have only 5 numbers.");
+                answer = int.Parse(Console.ReadLine());
+
+				if(answer.ToString().Length == 5)
+				{
+					PostCode = answer;
+                    Console.WriteLine("Great, your postcode saved.");
+                }
+            }
+        }
+    }
+}
+
